817079ae0542b67ab77b5239a7ec72c406a76374
Cheolmin Joo <cjfals4539@gmail.com>
2026-03-17 20:09:00 +0900

    Add Json Compatibility
    Json 호환성 Build.cs에 추가 및 Wordle에 사용할 json 추가

--- Source/CPP_Assignment_9_JCM/CPP_Assignment_9_JCM.Build.cs
+++ Source/CPP_Assignment_9_JCM/CPP_Assignment_9_JCM.Build.cs
@@ -8,7 +8,7 @@ public class CPP_Assignment_9_JCM : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
